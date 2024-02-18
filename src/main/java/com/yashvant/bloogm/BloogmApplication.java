2a2b45a7ee90590161b2d8d795ba41fe63c56a0d
yadavyashvant <yashvantyadav855@gmail.com>
2024-02-19 00:30:31 +0530

    Initial commit

---
+++ src/main/java/com/yashvant/bloogm/BloogmApplication.java
@@ -0,0 +1,13 @@
+package com.yashvant.bloogm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloogmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BloogmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yashvant/bloogm/BloogmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yashvant.bloogm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloogmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
