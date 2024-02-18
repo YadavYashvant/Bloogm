9b540328ff32b39b51c1facfd759ff3c74c711bc
yadavyashvant <yashvantyadav855@gmail.com>
2024-02-19 01:12:59 +0530

    fixed sql connection errors

--- src/main/java/com/yashvant/bloogm/BloogmApplication.java
+++ src/main/java/com/yashvant/bloogm/BloogmApplication.java
@@ -2,8 +2,9 @@ package com.yashvant.bloogm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication/*(exclude = {DataSourceAutoConfiguration.class})*/
 public class BloogmApplication {
 
     public static void main(String[] args) {
